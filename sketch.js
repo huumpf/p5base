dc8b3837dcc7f74a6e15f9727ab0267bf51c4ecc
huumpf <nico@windwalk.de>
2020-11-08 21:56:07 +0100

    initial commit, 2x4 tiles

--- sketch.js
+++ sketch.js
@@ -1,22 +1,50 @@
 var canvas;
-var looping = true;
+var x = 0;
+var y = 0;
 
 function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas = createCanvas(1920, 1080);
+  noLoop();
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  makeColors();
 }
 
-function keyTyped() {
-  if (key === " ") {
-    if (looping) {
-      noLoop();
-      looping = false;
-    } else {
-      loop();
-      looping = true;
-    }
+function makeColors() {
+  for (let i = 0; i < 4; i++) {
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
+    fill(r, g, b);
+    rect(x, y, width/4, height/2); 
+    x += width/4;
+  }
+
+  x = 0;
+  y += height/2;
+
+  for (let i = 0; i < 4; i++) {
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
+    fill(r, g, b);
+    rect(x, y, width/4, height/2); 
+    x += width/4;
+  }
+
+  x = 0;
+  y = 0;
+}
+
+function keyPressed() {
+  switch (key) {
+    case "s":
+      save();
+      break;
+    case "n":
+      makeColors();
+      break;
   }
 }
